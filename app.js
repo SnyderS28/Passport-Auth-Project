56aafa8cea4ef4e355db764c6bfae14db4ca8048
Steven Snyder <SnyderS28@users.noreply.github.com>
2021-07-22 17:38:40 -0600

    Starting to build routes

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+// Routes
+app.use('/', require('./routes/index'));
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`))
